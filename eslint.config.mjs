ba74836137cf6fbc7273402d473fc4993d8259c0
Andrei <andrei.lerke@gmail.com>
2026-06-21 22:02:34 +0200

    добавление es lint

---
+++ eslint.config.mjs
@@ -0,0 +1,64 @@
+import reactPlugin from '@eslint-react/eslint-plugin';
+import eslintJsPlugin from '@eslint/js';
+import eslintConfigPrettier from 'eslint-config-prettier';
+import prettierPlugin from 'eslint-plugin-prettier';
+import reactHooksPlugin from 'eslint-plugin-react-hooks';
+import { defineConfig } from 'eslint/config';
+import globalsPlugin from 'globals';
+import tseslintPlugin from 'typescript-eslint';
+
+export default defineConfig({
+  files: ['**/*.{js,ts,tsx}'],
+  extends: [
+    eslintJsPlugin.configs.recommended,
+    tseslintPlugin.configs.recommended,
+  ],
+  languageOptions: {
+    globals: {
+      ...globalsPlugin.node,
+      ...globalsPlugin.browser,
+    },
+    // parser: tseslintPlugin.parser,
+    // parserOptions: {
+    //   project: ['tsconfig.json', 'tsconfig.node.json'],
+    //   ecmaVersion: 'latest',
+    //   sourceType: 'module',
+    // },
+  },
+  plugins: {
+    '@typescript-eslint': tseslintPlugin.plugin,
+    react: reactPlugin,
+    'react-hooks': reactHooksPlugin,
+    prettier: prettierPlugin,
+  },
+  rules: {
+    ...eslintConfigPrettier.rules,
+    ...eslintJsPlugin.configs.recommended.rules,
+    'prefer-const': 'error',
+    'no-undef': 'off',
+    'no-console': 'warn',
+    'no-unused-vars': 'off',
+    'no-duplicate-imports': 'error',
+    'sort-imports': [
+      'error',
+      {
+        ignoreCase: false,
+        ignoreDeclarationSort: true,
+        ignoreMemberSort: true,
+        memberSyntaxSortOrder: ['none', 'all', 'multiple', 'single'],
+        allowSeparatedGroups: true,
+      },
+    ],
+    'react/prop-types': 0,
+    'react/jsx-indent': 0,
+    '@typescript-eslint/no-unused-vars': 'warn',
+    '@typescript-eslint/indent': 0,
+    '@typescript-eslint/no-empty-object-type': 'warn',
+    '@typescript-eslint/no-explicit-any': 'warn',
+    '@typescript-eslint/no-useless-empty-export': 'error',
+    '@typescript-eslint/no-mixed-enums': 'error',
+    '@typescript-eslint/no-require-imports': 'off',
+    '@typescript-eslint/prefer-enum-initializers': 'error',
+    'react-hooks/exhaustive-deps': 'warn',
+  },
+});
