ed8337533a793c5f129f320c9effb6fda7fa3e14
Andrei <andrei.lerke@gmail.com>
2026-06-21 23:32:23 +0200

    обновление темы

--- src/constants/theme.ts
+++ src/constants/theme.ts
@@ -7,12 +7,15 @@ import '@/global.css';
 
 import { Platform } from 'react-native';
 
-export enum TextColor {
+/** Цвет текста */
+export enum TintColor {
   DEFAULT = '#16171D',
+  WHITE = '#FFFFFF',
   ERROR = '#EC312F',
   DISABLED = '#AEB1C2',
 }
 
+/** Главный цвет приложения */
 export enum PrimaryColor {
   DEFAULT = '#115FFD',
   DISABLED = '#1160fd82',
