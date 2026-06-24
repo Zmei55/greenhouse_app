b86f30a226d9f8037db764f5d4d6c9dd59171b08
Andrei <andrei.lerke@gmail.com>
2026-06-24 23:40:34 +0200

    доб компонент формы настроек

---
+++ src/components/SettingsForm.tsx
@@ -0,0 +1,179 @@
+import { router } from 'expo-router';
+import { FC } from 'react';
+import { SubmitHandler, useForm } from 'react-hook-form';
+import { StyleSheet } from 'react-native';
+
+import { ButtonUI, InputUI, StackUI, SwitchUI, TextUI } from '@/components/ui';
+import { ButtonColor } from '@/constants/theme';
+import { useSettingsSend } from '@/hooks';
+import { SettingsFormType, SettingsType } from '@/types/settings.types';
+
+interface SettingsFormProps {
+  settings: SettingsType;
+}
+
+export const SettingsForm: FC<SettingsFormProps> = ({ settings }) => {
+  const { handleSettingsSending } = useSettingsSend();
+
+  /** Инициализируем форму. Цифры приводим к строкам для работы с <Input /> */
+  const { control, handleSubmit } = useForm<SettingsFormType>({
+    defaultValues: {
+      temperature: settings.sensors.temperature,
+      soilMoisture: settings.sensors.soilMoisture,
+      photo: settings.sensors.photo,
+      controlTemperature: settings.controlTemperature.toString(),
+      controlTime: settings.controlTime.toString(),
+      runningTime: settings.runningTime.toString(),
+      waterPressure: settings.watering.waterPressure.toString(),
+      soilDry: settings.watering.soil.dry.toString(),
+      soilWet: settings.watering.soil.wet.toString(),
+      workingHoursAktive: settings.workingHours.isEnabled,
+      startHour: settings.workingHours.start
+        ? settings.workingHours.start.hour.toString()
+        : null,
+      startMinute: settings.workingHours.start
+        ? settings.workingHours.start.minute.toString()
+        : null,
+      endHour: settings.workingHours.end
+        ? settings.workingHours.end.hour.toString()
+        : null,
+      endMinute: settings.workingHours.end
+        ? settings.workingHours.end.minute.toString()
+        : null,
+    },
+  });
+
+  const onSubmit: SubmitHandler<SettingsFormType> = data => {
+    handleSettingsSending(data);
+  };
+
+  return (
+    <StackUI spacing={6}>
+      <StackUI style={styles.sensorsContainer}>
+        <TextUI variant="subtitle">Датчики:</TextUI>
+        <StackUI style={styles.sensor} spacing={2}>
+          <TextUI>Термометр:</TextUI>
+          <SwitchUI value={false} />
+        </StackUI>
+
+        <StackUI style={styles.sensor} spacing={2}>
+          <TextUI>Влажность воздуха:</TextUI>
+          <SwitchUI value={false} />
+        </StackUI>
+
+        <StackUI style={styles.sensor} spacing={2}>
+          <TextUI>Освещенность:</TextUI>
+          <SwitchUI value={false} />
+        </StackUI>
+      </StackUI>
+
+      <StackUI spacing={2}>
+        <InputUI<SettingsFormType>
+          name="controlTemperature"
+          control={control}
+          label={'Контрольная температура (\u00B0C):'}
+        />
+        <InputUI<SettingsFormType>
+          name="controlTime"
+          control={control}
+          label="Контрольное время (сек):"
+        />
+        <InputUI<SettingsFormType>
+          name="runningTime"
+          control={control}
+          label="Время работы мотора (сек):"
+        />
+      </StackUI>
+
+      <StackUI>
+        <TextUI variant="subtitle">Полив:</TextUI>
+        <InputUI<SettingsFormType>
+          name="waterPressure"
+          control={control}
+          label="Давление воды (max: 225):"
+        />
+        <InputUI<SettingsFormType>
+          name="soilDry"
+          control={control}
+          label="Мокрая почва (max: 3500):"
+        />
+        <InputUI<SettingsFormType>
+          name="soilWet"
+          control={control}
+          label="Сухая почва (min: 2920):"
+        />
+      </StackUI>
+
+      <StackUI>
+        <TextUI variant="subtitle">Рабочие часы:</TextUI>
+        <StackUI style={styles.sensor} spacing={2}>
+          <TextUI>Активно:</TextUI>
+          <SwitchUI value={settings.workingHours.isEnabled} />
+        </StackUI>
+
+        {settings.workingHours.isEnabled && (
+          <StackUI>
+            <StackUI>
+              <TextUI>Начало:</TextUI>
+              {settings.workingHours.start && (
+                <StackUI direction="row" spacing={2} alignItems="center">
+                  <InputUI<SettingsFormType>
+                    name="startHour"
+                    control={control}
+                  />
+                  <TextUI variant="title">:</TextUI>
+                  <InputUI<SettingsFormType>
+                    name="startMinute"
+                    control={control}
+                  />
+                </StackUI>
+              )}
+            </StackUI>
+
+            <StackUI>
+              <TextUI>Окончание:</TextUI>
+              {settings.workingHours.end && (
+                <StackUI direction="row" spacing={2} alignItems="center">
+                  <InputUI<SettingsFormType> name="endHour" control={control} />
+                  <TextUI variant="title">:</TextUI>
+                  <InputUI<SettingsFormType>
+                    name="endMinute"
+                    control={control}
+                  />
+                </StackUI>
+              )}
+            </StackUI>
+          </StackUI>
+        )}
+      </StackUI>
+
+      <StackUI direction="row" spacing={4}>
+        <ButtonUI
+          onClick={() => router.back()}
+          colors={{ containerColor: ButtonColor.ERROR }}
+        >
+          Отмена
+        </ButtonUI>
+        <ButtonUI
+          onClick={handleSubmit(onSubmit)}
+          colors={{ containerColor: ButtonColor.PRIMARY }}
+        >
+          Сохранить
+        </ButtonUI>
+      </StackUI>
+    </StackUI>
+  );
+};
+
+const styles = StyleSheet.create({
+  sensorsContainer: {
+    width: '100%',
+    alignItems: 'center',
+  },
+  sensor: {
+    width: '75%',
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+  },
+});
