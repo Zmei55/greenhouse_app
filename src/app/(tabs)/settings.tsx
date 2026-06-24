fd67105c3bc6bb26fe3a32b2e384514b2c51dae1
Andrei <andrei.lerke@gmail.com>
2026-06-24 23:43:05 +0200

    доб страница настроек

--- src/app/(tabs)/settings.tsx
+++ src/app/(tabs)/settings.tsx
@@ -1,12 +1,43 @@
-import { FC } from 'react';
-import { Text, View } from 'react-native';
+import { useEffect } from 'react';
+import { ScrollView, StyleSheet } from 'react-native';
+import { SafeAreaView } from 'react-native-safe-area-context';
 
-const SettingsScreen: FC = () => {
+import { SettingsForm } from '@/components';
+import { StackUI, TextUI } from '@/components/ui';
+import { useAppSelector, useSettingsGet } from '@/hooks';
+import { isSettingsLoadingSelector, settingsSelector } from '@/redux/settings';
+
+const SettingsScreen: React.FC = () => {
+  const settings = useAppSelector(settingsSelector);
+  const isSettingsLoading = useAppSelector(isSettingsLoadingSelector);
+  const { handleSettingsGetting } = useSettingsGet();
+
+  useEffect(() => {
+    handleSettingsGetting();
+  }, [handleSettingsGetting]);
+
   return (
-    <View>
-      <Text>Страница настроек</Text>
-    </View>
+    <SafeAreaView style={styles.safeArea}>
+      <ScrollView>
+        {isSettingsLoading && (
+          <StackUI>
+            <TextUI>Получение настроек...</TextUI>
+          </StackUI>
+        )}
+
+        {settings && !isSettingsLoading && <SettingsForm settings={settings} />}
+      </ScrollView>
+    </SafeAreaView>
   );
 };
 
 export default SettingsScreen;
+
+const styles = StyleSheet.create({
+  safeArea: {
+    flex: 1,
+    paddingTop: -20,
+    paddingLeft: 10,
+    paddingRight: 10,
+  },
+});
